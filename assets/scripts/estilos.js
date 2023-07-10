957efaae8dc5becf7b6166a51f7c53ed158aecf3
helencristina19 <helensouzza8@gmail.com>
2023-07-10 11:45:20 -0300

    Alterações

---
+++ assets/scripts/estilos.js
@@ -0,0 +1,16 @@
+const styles = document.createElement('link');
+styles.setAttribute('rel','stylesheet');
+styles.setAttribute('href','./assets/styles/style.css');
+styles.setAttribute('type','text/css');
+
+//fontes do google
+const fontUbuntu = document.createElement("link");
+fontUbuntu.rel = 'stylesheet';
+fontUbuntu.href = 'https://fonts.googleapis.com';
+
+
+
+
+    //link rel = "stylesheet" href = "./assets/style/styles.css">
+
+    document.head.appendChild(styles);
