c3b5c5040bfa7f46310c534e98048a0e8871c41c
helencristina19 <helensouzza8@gmail.com>
2023-07-11 11:46:30 -0300

    Alterações

--- assets/scripts/estilos.js
+++ assets/scripts/estilos.js
@@ -3,14 +3,23 @@ styles.setAttribute('rel','stylesheet');
 styles.setAttribute('href','./assets/styles/style.css');
 styles.setAttribute('type','text/css');
 
-//fontes do google
-const fontUbuntu = document.createElement("link");
-fontUbuntu.rel = 'stylesheet';
-fontUbuntu.href = 'https://fonts.googleapis.com';
+    // <link rel = "stylesheet" href = "./assets/style/styles.css">
 
 
+const link1 = document.createElement('link');
+link1.rel = "preconnect";
+link1.href = "https://fonts.googleapis.com"; 
+
+const link2 = document.createElement('link');
+link2.rel = "preconnect";
+link2.href ="https://fonts.googleapis.com";
 
 
-    //link rel = "stylesheet" href = "./assets/style/styles.css">
+const link3 = document.createElement('link');
+link3.rel = "preconnect";
+link3.href = "https://fonts.googleapis.com";
 
-    document.head.appendChild(styles);
+
+    
+document.head.appendChild(styles);
+document.head.append(link1,link2,link3)
